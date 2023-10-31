b1a0b6037b8fe959d50e38b00a7445e6d895844b
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-10-31 15:28:50 +0100

    added create_note

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 from decorators.input_error import input_error
+from decorators.note_error import note_error
 from models.address_book import AddressBook
+from models.note import Note, Notes, Tag
 from models.record import Record
+from utils.contacts import dump_notes, load_notes
+
+
+notes_json = 'notes.json'
+contacts_json = 'contacts.json'
 
 
 class Bot:
     def __init__(self):
         self.addressBook = AddressBook()
+        self.notes = Notes()
 
     def parse_input(self, user_input):
         cmd, *args = user_input.split()
@@ -55,14 +63,30 @@ class Bot:
     def birthdays(self):
         return self.addressBook.get_birthdays_per_week()
 
+    @note_error
+    def create_note(self, args):
+        title = args[0]
+
+        text = input("Input Note text: ")
+        raw_tags = input("Input tags like tag1,tag2,tag3... : ")
+
+        tags = [Tag(tag.strip()) for tag in raw_tags.split(',')]
+        note = Note(title=title, text=text, tags=tags)
+
+        self.notes.add_note(note)
+        dump_notes(notes_json, self.notes.to_dict())
+
+        return "Note '{}' successfully added".format(note.title)
+
     def run(self):
         print("Welcome to the assistant bot!")
+
         while True:
             user_input = input("Enter a command: ")
             command, *args = self.parse_input(user_input)
 
             try:
-                if command in ["close", "exit"]:
+                if command in ["close", "exit", "bye"]:
                     print("Good bye!")
                     break
                 elif command == "hello":
@@ -81,7 +105,20 @@ class Bot:
                     print(self.show_birthday(args))
                 elif command == "birthdays":
                     print(self.birthdays())
+                elif command == "add-note":
+                    print(self.create_note(args))
+                elif command == "show-notes":
+                    print(self.show_notes())
+                elif command == "find-note":
+                    print(self.find_note(args))
+                elif command == "delete-note":
+                    print(self.delete_note(args))
+                elif command == "get-note":
+                    print(self.get_note(args))
+                elif command == "update-note":
+                    print(self.update_note(args))
                 else:
                     print("Invalid command.")
             except Exception:
                 print("Invalid arguments for command:", command)
+
